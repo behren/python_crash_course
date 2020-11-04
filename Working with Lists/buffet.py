a305ebf6368358cd37b4ac88c24b1fe7bfbbe72a
ragemoody <mobile.niclas@mail.com>
2020-11-04 21:01:07 +0100

    added buffet.py

---
+++ Working with Lists/buffet.py
@@ -0,0 +1,9 @@
+foods = ('spaghetti', 'fish', 'salad', 'pizza', 'mushrooms')
+
+for food in foods:
+    print(f"{food}")
+
+foods = ('cucumber', 'beef', 'salad', 'pizza', 'mushrooms')
+for food in foods:
+    print(f"{food}")
+    
