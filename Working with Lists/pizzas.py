5246ca09902e7e2ef0d4f120e991c41ab7075559
ragemoody <mobile.niclas@mail.com>
2020-11-04 19:53:22 +0100

    added friends_pizza to pizza.py

--- Working with Lists/pizzas.py
+++ Working with Lists/pizzas.py
@@ -1,5 +1,6 @@
 pizzas = ['salami', 'hawaii', 'san siro', 'pepperoni', 'tonno']
 
+
 for pizza in pizzas:
     print(f"Ich mag {pizza.title()} Pizza")
 
@@ -14,3 +15,16 @@ print(pizzas[1:4])
 
 print("The last three items in the list are:")
 print(pizzas[2:5])
+
+friend_pizzas = pizzas[:]
+
+pizzas.append("funghi")
+friend_pizzas.append("margherita")
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(f"{pizza}")
+
+print("My friends favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(f"{pizza}")
