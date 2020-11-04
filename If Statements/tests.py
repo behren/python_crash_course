c5ddb4f0b12cdfadb84bfd3f993a8ed2bb05018e
ragemoody <mobile.niclas@mail.com>
2020-11-04 22:05:58 +0100

    added tests.py

---
+++ If Statements/tests.py
@@ -0,0 +1,47 @@
+car = 'mercedes'
+
+print("Is car == mercedes? I predict True")
+print(car == 'mercedes')
+
+print("Is car == subaru? I predict false")
+print(car == 'subaru')
+
+food = 'spaghetti'
+
+print('I bet food == spaghetti')
+print(food == 'spaghetti')
+
+print('I bet food != mushrooms')
+print(food == 'mushrooms')
+
+club = 'bvb'
+
+print('I bet club == bvb!')
+print(club == 'bvb')
+
+print('I bet club != fcb')
+print(club == 'fcb')
+
+number = 42
+
+print('Is number > 40? I bet yes')
+print(number > 40)
+
+print('Is number < 50? I bet yes')
+print(number < 50)
+
+goals = 5
+
+print('Is goals < 3? I bet yes')
+print(goals < 3)
+
+print('Is goals > 5? I bet yes')
+print(goals > 5)
+
+player = 'hazard'
+
+print('Is player != Hummels? I bet True')
+print(player != 'Hummels')
+
+print('Is player == Hazard? I bet True')
+print(player.title() == 'Hazard')
