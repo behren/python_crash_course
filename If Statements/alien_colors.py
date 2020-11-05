249c82e765a9e460625de1c15fe43529ad4beb64
Niclas Behrendt <niclas.behrendt@msg.group>
2020-11-05 19:57:40 +0100

    added if/elif statements to alien_colors.py

--- If Statements/alien_colors.py
+++ If Statements/alien_colors.py
@@ -5,3 +5,35 @@ if alien_color == 'yellow':
 
 if alien_color == 'green':
     print("You earned 10 points!")
+
+alien_color = 'blue'
+
+if alien_color == 'yellow':
+    print('You earned 5 points!')
+else:
+    print('You earned 10 points!')
+
+alien_color = 'blue'
+
+if alien_color == 'blue':
+    print('You earned 5 points!')
+else:
+    print('You earned 10 points!')
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You earned 5 points!")
+elif alien_color == 'blue':
+    print("You earned 10 points!")
+elif alien_color == 'red':
+    print("You earned 20 points!")
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You earned 5 points!")
+elif alien_color == 'blue':
+    print("You earned 10 points!")
+elif alien_color == 'red':
+    print("You earned 20 points!")
