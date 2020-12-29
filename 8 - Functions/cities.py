9bbfb8314ef808a899d0c0364fb8eb774c7c1038
Niclas Behrendt <niclas.behrendt@msg.group>
2020-12-29 08:45:54 +0100

    finished 8-3, 8-4, 8-5

---
+++ 8 - Functions/cities.py
@@ -0,0 +1,7 @@
+def describe_city(city, country='England'):
+
+    print(f"{city.title()} is in {country.title()}")
+
+describe_city('london')
+describe_city('birmingham')
+describe_city('mölln', 'germany')
---
+++ 8 - Functions/tshirt.py
@@ -0,0 +1,8 @@
+def make_shirt(size='l', message='I love Python'):
+
+    print(f"The size of your shirt will be {size.upper()}.")
+    print(f"The text message printed on it will be '{message}'.")
+
+make_shirt()
+make_shirt('m')
+make_shirt('s', 'I love Spaghetti')
