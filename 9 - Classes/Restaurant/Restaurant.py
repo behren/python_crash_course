790f4bef7dd37a88e073e9bba3a0f01e90763786
ragemoody <mobile.niclas@mail.com>
2021-01-03 00:15:54 +0100

    solved 9-4 and 9-5

--- 9 - Classes/Restaurant/Restaurant.py
+++ 9 - Classes/Restaurant/Restaurant.py
@@ -2,11 +2,12 @@ class Restaurant:
     """
     A class to describe a restaurant.
     """
-    
+
     def __init__(self, name, cuisine):
         """Initialize the restaurant attributes."""
         self.restaurant_name = name.title()
         self.cuisine_type = cuisine.title()
+        self.number_served = 0
 
     def describe_restaurant(self):
         """Describe the restaurant by it's name and cuisine type."""
@@ -16,15 +17,22 @@ class Restaurant:
         """Opens the restaurant."""
         print(f"{self.restaurant_name} is now open!")
 
-restaurant = Restaurant('McDonalds', 'fastfood')
-print(restaurant.restaurant_name)
-print(restaurant.cuisine_type)
+    def set_number_served(self, number):
+        """Sets the number of served items."""
+        self.number_served = number
 
+    def increment_number_served(self, add_served):
+        """Increments the number of served items."""
+        self.number_served += add_served
+
+
+restaurant = Restaurant("McDonalds", "fastfood")
 restaurant.describe_restaurant()
+print(f"Numbers served: {restaurant.number_served}.")
 restaurant.open_restaurant()
-
-restaurant2 = Restaurant('Bullerei', 'star cook')
-restaurant2.describe_restaurant()
-
-restaurant3 = Restaurant('Lavastein', 'italian')
-restaurant3.describe_restaurant()
+restaurant.number_served = 42
+print(f"Numbers served: {restaurant.number_served}.")
+restaurant.set_number_served(1337)
+print(f"Numbers served: {restaurant.number_served}.")
+restaurant.increment_number_served(420_000)
+print(f"Numbers served: {restaurant.number_served}.")
--- 9 - Classes/User/User.py
+++ 9 - Classes/User/User.py
@@ -2,25 +2,51 @@ class User:
     """
     Describes and greets a user.
     """
-    def __init__ (self, first, last, age, location):
+
+    def __init__(self, first, last, age, location):
         """Initialize user attributes."""
         self.first_name = first.title()
         self.last_name = last.title()
         self.age = age
         self.location = location.title()
+        self.login_attempts = 0
 
     def greet_user(self):
         """Greets the user by name."""
-        msg = f"Hello, {self.first_name} {self.last_name}, how are you? Here" \
-              f" is a summary of what we know about you:"
+        msg = (
+            f"Hello, {self.first_name} {self.last_name}, how are you? Here"
+            f" is a summary of what we know about you:"
+        )
         print(msg)
 
     def describe_user(self):
         """Describes the user with all attributes."""
-        summary = f"Your name is {self.first_name} {self.last_name}, you are" \
-                    f" {self.age} years old and located in {self.location}."
+        summary = (
+            f"Your name is {self.first_name} {self.last_name}, you are"
+            f" {self.age} years old and located in {self.location}."
+        )
         print(summary)
 
-me = User('Niclas', 'Behrendt', '30', 'hamburg')
+    def increment_login_attempts(self):
+        """Increments the login attempts by 1."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Resets the login attempts back to 0."""
+        self.login_attempts = 0
+
+
+me = User("Niclas", "Behrendt", "30", "hamburg")
 me.greet_user()
 me.describe_user()
+print(f"Login attempts: {me.login_attempts}")
+me.increment_login_attempts()
+print(f"Login attempts: {me.login_attempts}")
+me.increment_login_attempts()
+print(f"Login attempts: {me.login_attempts}")
+me.increment_login_attempts()
+print(f"Login attempts: {me.login_attempts}")
+me.increment_login_attempts()
+print(f"Login attempts: {me.login_attempts}")
+me.reset_login_attempts()
+print(f"Login attempts: {me.login_attempts}")
