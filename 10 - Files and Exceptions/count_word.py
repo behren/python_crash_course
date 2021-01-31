383e1f378fd540a73dc5ab4a4bb886a49961f9a3
behren <niclas.behrendt@msg.group>
2021-01-31 18:24:34 +0100

    solved 10-10

---
+++ 10 - Files and Exceptions/count_word.py
@@ -0,0 +1,11 @@
+filename = "DerSchatzImSilbersee.txt"
+word = "die"
+
+try:
+    with open(filename, encoding="utf-8") as f:
+        content = f.read()
+except:
+    FileNotFoundError(print(f"The file '{filename}' does not exist"))
+else:
+    occurances = content.lower().count(word)
+    print(f"The word '{word}' occured {occurances} times.")
