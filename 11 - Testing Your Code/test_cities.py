693bce3a0dd5df34d3ccb8173f041d4ef9cffe17
behren <niclas.behrendt@msg.group>
2021-02-03 06:59:38 +0100

    added comments to describe CityTestCases functions

--- 11 - Testing Your Code/test_cities.py
+++ 11 - Testing Your Code/test_cities.py
@@ -6,10 +6,12 @@ class CityTestCases(unittest.TestCase):
     """Testing the city_country function"""
 
     def test_city_country(self):
+        """Testing the function with only the city and country variable"""
         city_country = get_city_country("hamburg", "germany")
         self.assertEqual(city_country, "Hamburg, Germany")
 
     def test_city_country_population(self):
+        """Testing the function, including the population variable"""
         city_country_population = get_city_country(
             "hamburg", "germany", population="1.850.000"
         )
