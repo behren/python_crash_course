998f6e47df7f871043bd4181c0e40d0bf14a2671
Niclas Behrendt <behren@users.noreply.github.com>
2021-02-08 16:37:26 +0100

    changed salary to float

--- 11 - Testing Your Code/employee.py
+++ 11 - Testing Your Code/employee.py
@@ -8,15 +8,16 @@ class Employee:
         self.salary = salary
 
     def describe_employee(self):
+        self.salary = float(self.salary)
         employee = f"{self.lastname}, {self.firstname} - salary: ${self.salary:,}"
         return f"{employee.title()}"
 
     def give_raise(self, raise_by="5000"):
-        self.new_salary = int(self.salary) + int(raise_by)
-        self.salary = f"{self.new_salary:,}"
+        self.new_salary = float(self.salary) + float(raise_by)
+        self.salary = f"{self.new_salary}"
 
 
-me = Employee("niclas", "behrendt", "500000")
+me = Employee("niclas", "behrendt", "500000.50")
 print(me.describe_employee())
 me.give_raise()
 print(me.describe_employee())
